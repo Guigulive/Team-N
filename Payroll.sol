b9be3d1d16f23c42d9fd51e43150a752cac0efb9
RockYu <softwareyhl@gmail.com>
2018-03-17 19:08:31 +0800

    my homework for first lesson

--- Payroll.sol
+++ Payroll.sol
@@ -1,1 +1,40 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+contract Payroll {
+
+    address employee;
+    uint salary;
+    uint constant payDuration = 1 seconds;
+    uint lastPayDay;
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunOut() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunOut() > 0;
+    }
+
+    function getPaid() payable returns (uint){
+        if (msg.sender != employee) {
+            revert();
+        }
+        uint nextPayDay = lastPayDay + payDuration;
+        if (nextPayDay > now) {
+            revert();
+        }
+        lastPayDay = nextPayDay;
+        employee.transfer(salary);
+        return this.balance;
+    }
+    
+    function udpateEmployee(address e, uint s) { /**here address parameter need to add double quote on remix **/
+       
+        employee = e;
+        salary = s * 1 ether;
+        lastPayDay = now;
+    } 
+}
