f564f2b8af09a902bb0ea6b5d4370c6aa6dd317e
Yuan Jiang <jiangyuan916@gmail.com>
2018-03-24 15:17:14 -0700

    assignment 1

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,51 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    uint salary;
+    address employee;
+    uint lastPayday;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+
+    function updateEmployee(address e, uint s) {
+        require(msg.sender == owner);
+        employee = e;
+        salary = s * 1 ether;
+        lastPayday = now;
+
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+    }
+
+    function addFund() payable returns (uint) {
+        require(msg.sender == owner);
+        return this.balance;
+    }
+
+    function getPaid() {
+        require(msg.sender == employee);
+
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+
+    function updatePayDuration(uint payDuration) {
+        require(msg.sender == owner);
+        this.payDuration = payDuration;
+        lastPayday = now;
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+    }
+}
